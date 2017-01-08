2d3f14441c4b00ada5eacce804726fa5c37de5a9
BenjaminThomas1999 <bentheambassador@outlook.com>
2017-01-08 18:46:12 +0000

    Update HardwareIO.py

--- HardwareIO.py
+++ HardwareIO.py
@@ -1,8 +1,13 @@
-import time, os, thread 
+import time
+from threading import Timer
 import RPi.GPIO as GPIO
  
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(24, GPIO.OUT)
+
+def cycle(arr, item):
+	arr.append(item)
+	return arr[1:]
 
 all_inputs = []
 class HwInput(object):
@@ -13,7 +18,10 @@ class HwInput(object):
 		all_inputs.append(self)		
 		self.history = [0, 0]	
 
-	def state(self):
+	def state(self):#state() returns the change of input
+			#True = [0, 1] = stepped on
+			#False = [1, 0] = stepped off
+			#None = No change
 		if not self.history[0] == self.history[1]:
 			if self.history[1] == 1:
 				return True
@@ -23,74 +31,75 @@ class HwInput(object):
 			return None
 	
 	def update(self):
-		self.history[0] = self.history[1]
-		self.history[1] = GPIO.input(self.PIN) 
-
-	
-sequence = ["" , "", "", ""]
+		self.history = cycle(self.history, GPIO.input(self.PIN))
+		
+sequence = []
+for i in range(2):
+	sequence.append("")
 
 door_sensor = HwInput(18, "door_sensor")
 pressure_plate = HwInput(23, "pressure_plate")
 wall_pad = HwInput(25, "wall_pad")
 main_switch = HwInput(4, "main_switch")
-light_state = True
+door_laser = HwInput(17, "door_laser")
+light_state = True #light is on by default
+laser_state = False #laser is not tripped by default
 
-def cycle(arr, item):
-	arr.append(item)
-	return arr[1:]
+
+def coolDownFunction():
+	global sequence
+	print("sequence cleared due to cool down")
+	sequence = ["", ""]
+
+def restartCoolDownTimer():
+	global cool_down_timer
+	cool_down_timer.cancel()
+	cool_down_timer = Timer(8, coolDownFunction)
+	cool_down_timer.start()
+	
+cool_down_timer = Timer(8, coolDownFunction)
+
+
 
 def toggleLight():
 	global light_state
 	light_state = not light_state	
 
 def ioUpdate():
-	global light_state, sequence
-	for i in all_inputs:#updates input states
+	global light_state, sequence, cool_down_timer
+	for i in all_inputs:
 		i.update()
 	
-	if wall_pad.state() == True:
+	print(sequence)
+		
+	if wall_pad.state() == True:#When wall pad is pressed toggle light
 		toggleLight()
-	if main_switch.state() == True or main_switch.state() == False:
-		light_state = not light_state	
+	if main_switch.state() == True or main_switch.state() == False:#when light switch changes, toggle light
+		toggleLight()
 	
-	
-	if door_sensor.state() == True:
-		sequence = cycle(sequence, "DC")#DC = Door Closed
-	elif door_sensor.state() == False:
-		sequence = cycle(sequence, "DO")#DO = Door Opened
-	elif pressure_plate.state() == True:
+	if pressure_plate.state() == True:
 		sequence = cycle(sequence, "PA")#PA = Plate Activated
-	elif pressure_plate.state() == False:
-		sequence = cycle(sequence, "PO")#PO = Plate Off	
+		restartCoolDownTimer()
+
+	elif door_laser.state() == True:
+		sequence = cycle(sequence, "LT")#LT = Laser Triggered		
+		restartCoolDownTimer()
+
+
+
+	if sequence == ["LT", "PA"]:
+		if light_state == False:		
+			light_state = True
+		#sequence = ["", ""]
 		
-
-	if sequence == ["DO", "PA", "PO", "DC"]:#could be walking in or out. Invert light state
-		light_state = not light_state
-		sequence = ["", "", "", ""]
-	elif sequence == ["DO", "DC", "PA", "PO"]:
-		light_state = True
-		sequence = ["", "", "", ""]
-
-	elif sequence == ["DO", "PA", "DC", "PO"]:
-		light_state = True
-		sequence = ["", "", "", ""]
-
-	elif sequence == ["PA", "PO", "DO", "DC"]:
-		light_state = False
-		sequence = ["", "", "", ""]
-
-	elif sequence == ["PA", "DO", "PO", "DC"]:
-		light_state = False
-		sequence = ["", "", "", ""]
-	elif sequence == ["DO", "DC", "DO", "DC"]:
-		light_state = False
-		sequence = ["", "", "", ""]
-	elif sequence == ["PA", "PO", "PA", "PO"]:
-		light_state = True
-		sequence = ["", "", "", ""]
+	elif sequence == ["PA", "LT"]:
+		if light_state == True:
+			light_state = False
+		#sequence = ["", ""]
 		
-		
-	web_file = open("/var/www/html/output.txt")
+				
+	
+	web_file = open("/var/www/html/output.txt")#output from web client
 	web_input = web_file.read()
 	if web_input == "1":
 		light_state = True
@@ -101,7 +110,7 @@ def ioUpdate():
 	web_file = open("/var/www/html/output.txt", "w")#clear file
 	web_file.close()
 	
-	web_output = open("/var/www/html/state.txt", "w")
+	web_output = open("/var/www/html/state.txt", "w")#output to web client
 	
 	if light_state == True:
 		GPIO.output(24, False)
@@ -111,10 +120,12 @@ def ioUpdate():
 		web_output.write("off")
 	
 	web_output.close()
+
+
 def ioUpdateLoop():
 	while True:
 		ioUpdate()
-		time.sleep(0.1)
+		time.sleep(0.025)
 
 
 if __name__ == '__main__':
